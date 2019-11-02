315742d3f5943aef244d970ea7ca3d3a4307bd0f
Kristell Urueta <kristell.urueta@gmail.com>
2019-11-02 16:28:33 -0500

    Con menu

--- src/user/User.java
+++ src/user/User.java
@@ -13,21 +13,28 @@ public class User {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
+
+        int op;
+
+        do {
+            Scanner sc = new Scanner(System.in);
+
             System.out.print("Ingrese nombre de usuario: ");
             String user = sc.nextLine();
-            Pattern pattern = Pattern.compile("^@([A-Za-z0-9_]{1,15})$");
+            Pattern pattern = Pattern.compile("^@([A-Za-z0-9-_]{1,15})$");
             Matcher matcher = pattern.matcher(user);
             System.out.print(user);
 
             if (matcher.matches()) {
-                System.out.println(" ->Nombre validado correctamente");
+                System.out.println("  ->Usuario ingresado correcto.");
             } else if (!matcher.matches()) {
-                System.out.println(" ->Nombre incorrecto");
+                System.out.println("  ->Usuario ingresado incorrecto");
             } else if (user.equals("")) {
                 System.out.println(" ->Debe introducir nombre: ");
-            }    
-        }
+            }
+            System.out.println("1. Continuar    2. Salir");
+            op = sc.nextInt();
+        } while (op == 1);
+
     }
 }
